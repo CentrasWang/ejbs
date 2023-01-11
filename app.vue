96acdeee8ef4a8ad0502e17f52d6a4a785169202
CentrasW <centras@qq.com>
2023-01-12 04:42:47 +0800

    1

---
+++ app.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>壹家播商</h1>
+  <h2>壹家购</h2>
+  <div>拖欠员工工资，欠薪不给。</div>
+</template>
+<script setup lang="ts">
+useHead({
+  title:'壹家播商',
+  meta:[
+    {name: '壹家播商', content: '壹家播商，壹家购拖欠员工工资' }
+  ]
+})
+</script>
+<style lang="scss">
+h1{
+  font-size: 100px;
+}
+h2{
+  font-size: 100px;
+}
+div{
+  font-size: 200px;
+  color: red;
+}
+</style>
